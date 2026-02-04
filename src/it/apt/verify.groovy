7b14bbe2334013e2e16e4f94c29828fe5a64da98
Thor <Thor.Asgard.Dev@gmail.com>
2026-02-04 12:08:08 +0100

    Add changes file + update it verify script

--- src/it/apt/verify.groovy
+++ src/it/apt/verify.groovy
@@ -1,1 +1,2 @@
 assert new File( basedir, 'target/apt/pool/main/j/jdeb-it/jdeb-it_1.0_all.deb' ).exists();
+assert new File( basedir, 'target/apt/pool/main/t/test-package-zstd/test-package-zstd.deb' ).exists();
